d730b653391c1c5818dfcfc0ff7dadd644787d2f
FaustoFagner <faustofagner@gmail.com>
2015-10-03 11:01:18 -0300

    Criando classe executável com hello world...

---
+++ FigurasGeometricas/src/main/java/br/edu/fasam/figurasgeometricas/FigurasApp.java
@@ -0,0 +1,12 @@
+
+package br.edu.fasam.figurasgeometricas;
+
+/**
+ *
+ * @author Administrador
+ */
+public class FigurasApp {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    }
+}
