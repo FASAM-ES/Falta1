b0b89cd44d5431e5897e7db39f86467e6e122f4d
FaustoFagner <faustofagner@gmail.com>
2015-10-03 14:39:11 -0300

    Criando classe executável com hello world...

--- FigurasGeometricas/src/main/java/br/edu/fasam/figurasgeometricas/FigurasApp.java
+++ FigurasGeometricas/src/main/java/br/edu/fasam/figurasgeometricas/FigurasApp.java
@@ -7,27 +7,11 @@ package br.edu.fasam.figurasgeometricas;
  *
  * @author Administrador
  */
-import java.util.Scanner;
+
 public class FigurasApp {
     public static void main(String[] args) {
-     
-         
         System.out.println("Olá Mundo!");
-        Scanner entrada = new Scanner(System.in);
-        Circulo c = new Circulo();
-         System.out.println("Informe a operação desejada"+
-                "\n3-Classe Círculo");
-        int menu = entrada.nextInt();
-
-        switch (menu) {
-            case 1:
-                c.construtor();
-                c.imprimir();
-                break;
-            default:
-                System.out.println("Opção incorreta");
-            break;
-    }
+        
 }
 }
 
